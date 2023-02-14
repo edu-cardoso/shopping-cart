0d252ec6d2d21242590fb32f1e691660bf2b059e
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-02-14 15:51:01 -0300

    requisito 03

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+async function appendProduct() {
+  const products = document.querySelector('.products');
+  const data = await fetchProductsList('computador');
+  data.forEach((product) => {
+    products.appendChild(createProductElement({
+      id: product.id,
+      title: product.title,
+      thumbnail: product.thumbnail,
+      price: product.price,
+    }));
+  });
+}
+
+window.onload = async () => {
+  await appendProduct();
+};
