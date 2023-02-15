b6fed3e543022ffab4f4895372b14123a276ed68
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-02-14 22:57:16 -0300

    requisito 01

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,19 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
+    it('fetchProduct é uma função', () => {
+      expect(typeof fetchProduct).toBe('function');
+    });
+
+    it('fetch é chamado ao executar fetchProduct', async () => {
+      await fetchProduct('MLB1405519561');
+      expect(fetch).toHaveBeenCalled();
+    });
+
+    it('fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+      await fetchProduct('MLB1405519561');
+      expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+    });
+
     
-  });
 });
